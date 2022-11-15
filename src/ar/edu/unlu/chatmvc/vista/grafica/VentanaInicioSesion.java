d62b0da6511e82e93c887f24f7aad79d4fa8d558
federicoradeljak <federico.radeljak@gmail.com>
2022-11-15 12:02:56 -0300

    Arregla error que hacía que no abriera la ventana de inicio de sesión

--- src/ar/edu/unlu/chatmvc/vista/grafica/VentanaInicioSesion.java
+++ src/ar/edu/unlu/chatmvc/vista/grafica/VentanaInicioSesion.java
@@ -24,7 +24,6 @@ public class VentanaInicioSesion extends JFrame {
 	 * Create the frame.
 	 */
 	public VentanaInicioSesion() {
-		setType(Type.POPUP);
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 247, 109);
